77cff3b29f6465614d9d9a138777a143484a5fd2
kiryanchi <kiryanchi@naver.com>
2022-12-28 10:27:20 +0900

    initial commit

---
+++ src/main/java/com/appdong/parting/PartingApplication.java
@@ -0,0 +1,13 @@
+package com.appdong.parting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appdong/parting/PartingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdong.parting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
